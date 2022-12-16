a538da033f11249ee58a41c6ebf4ce27918f8d05
Artem <you@example.com>
2022-12-16 13:51:04 +0200

    add games

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import {Routes, Route, Navigate} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from "./components/base/NavBar";
+import HomePage from "./components/base/HomePage";
+import StatisticPage from "./components/base/StatisticPage";
+import SudokuApp from "./components/sudoku/SudokuApp";
+import TicTacToeApp from "./components/ticTacToe/TicTacToeApp";
+import {LogIn, SignUp} from "./components/auth/AuthPages";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+import SeaBattleApp from "./components/ seaBattle/SeaBattleApp";
+import "./App.css"
+
+import {PAGE_LINKS, AUTH_LINKS, back_req} from "./Common";
+
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+        }
+    }
+
+    componentDidMount() {
+        let username = localStorage.getItem("username");
+        let token = localStorage.getItem("Authorization");
+        if (token && username) {
+            back_req.defaults.headers.common["Authorization"] = token;
+            this.setState({username: username});
+        }
+    }
+
+    back_request() {
+        return back_req;
+    }
+
+    authenticate(data) {
+        let token = "Token " + data.token;
+        back_req.defaults.headers.common["Authorization"] = token;
+        localStorage.setItem("Authorization", token);
+        localStorage.setItem("username", data.username);
+        this.setState({username: data.username});
+    }
+
+    logOut() {
+        delete back_req.defaults.headers.common["Authorization"];
+        localStorage.removeItem("Authorization");
+        localStorage.removeItem("username");
+        this.setState({username: null});
+    }
+
+
+
+  render() {
+    return(
+        <div>
+
+            <NavBar user={ this.state.username } logout={ () => this.logOut() } />
+
+            <Routes>
+                <Route path={PAGE_LINKS.home} element={<HomePage/>} />
+                <Route path={PAGE_LINKS.statistic}
+                       element={<StatisticPage
+                           user={this.state.username}
+                           back={ () => this.back_request() }
+                       />}
+                />
+                <Route path={PAGE_LINKS.sudoku} element={<SudokuApp user={this.state.username} />} />
+                <Route path={PAGE_LINKS.tic_tac_toe} element={<TicTacToeApp/>} />
+                <Route path={PAGE_LINKS.sea_battle} element={<SeaBattleApp/>} />
+                <Route path={AUTH_LINKS.log_in}
+                       element={<LogIn
+                           auth={ data => this.authenticate(data) }
+                           user={ this.state.username }
+                       />}
+                />
+                <Route path={AUTH_LINKS.sign_up}
+                       element={<SignUp
+                           auth={ data => this.authenticate(data) }
+                           user={ this.state.username }
+                       />}
+                />
+            </Routes>
+
+        </div>
+    );
+  }
 }
 
-export default App
+
+export default App;
---
+++ src/Common.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+
+const HOME_LINK = "/";
+// const BACK_URL = "http://127.0.0.1:8000/";
+const BACK_URL = "http://localhost:8000/";
+// const BACK_URL = "https://games-production-7379.up.railway.app/";
+
+const back_req = axios.create({
+    "withCredentials": true,
+    "baseURL": BACK_URL,
+});
+
+const BACK_REQ_LINKS = {
+    "sign_up": "sign_up/",
+    "log_in": "log_in/",
+    "statistic": "statistic/",
+};
+
+const SUDOKU_LINKS = {
+    "base": "sudoku/",
+    "easy": "sudoku/easy/",
+    "medium": "sudoku/medium/",
+    "hard": "sudoku/hard/",
+};
+
+const TIC_TAC_TOE_LINKS = {
+    "base": "tic-tac-toe/",
+}
+
+const PAGE_LINKS = {
+    "home": HOME_LINK,
+    "statistic": HOME_LINK + "statistic/",
+    "sudoku": HOME_LINK + "sudoku/",
+    "tic_tac_toe": HOME_LINK + "tic-tac-toe/",
+    "sea_battle": HOME_LINK + "sea_battle/",
+};
+
+const AUTH_LINKS = {
+    "log_in": HOME_LINK + "log_in/",
+    "sign_up": HOME_LINK + "sign_up/",
+};
+
+export {PAGE_LINKS, AUTH_LINKS, SUDOKU_LINKS, TIC_TAC_TOE_LINKS, BACK_REQ_LINKS, back_req};
